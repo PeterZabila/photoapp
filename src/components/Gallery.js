7df13496b12eb2bee31bedc5c6c56b343911a3fe
PeterZabila <petrozabila@gmail.com>
2023-06-21 17:29:49 +0300

    added photos to album

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,50 +1,59 @@
 import React from 'react';
-import { ImageList, ImageListItem } from '@material-ui/core';
+import { ImageList, ImageListItem, Box } from '@material-ui/core';
+import Container from '@mui/material/Container';
 import { v4 as uuidv4 } from 'uuid';
 import img from '../images/wedding1/01.jpg'
+import img1 from '../images/wedding1/02.jpg'
+import img2 from '../images/wedding1/03.jpg'
+import img3 from '../images/wedding1/04.jpg'
+import img4 from '../images/wedding1/05.jpg'
+import img5 from '../images/wedding1/06.jpg'
 
 const images = [
     {
         id: uuidv4(),
-        image: '../images/wedding1/06.jpg'
+        loc: img
     },
     {
         id: uuidv4(),
-        image: '../images/wedding1/01.jpg'
+        loc: img1
     },
     {
         id: uuidv4(),
-        image: '../images/wedding1/02.jpg'
+        loc: img2
     },
     {
         id: uuidv4(),
-        image: '../images/wedding1/03.jpg'
+        loc: img3
     },
     {
         id: uuidv4(),
-        image: '../images/wedding1/04.jpg'
+        loc: img4
     },
     {
         id: uuidv4(),
-        image: '../images/wedding1/05.jpg'
+        loc: img5
     },
 ]
 
 const Gallery = () => {
   return (
-
-    <ImageList variant="masonry" cols={3} gap={8}>
-        <img src={img} />
-        {images.map((item) => (
-            <ImageListItem key={item.id}>
-            <img
-                src={item.image}
-                alt={item.id}
-                loading="lazy"
-            />
-            </ImageListItem>
-        ))}
-    </ImageList>
+    <Container>
+        <Box>
+        <ImageList variant="masonry" cols={3} gap={8}>
+            {images.map(item => (
+                <ImageListItem key={item.id}>
+                    <img
+                        src={item.loc}
+                        alt={item.id}
+                        loading="lazy"
+                    />
+                </ImageListItem>
+            ))}
+        </ImageList>
+    </Box>
+    </Container>
+    
   )
 }
 
